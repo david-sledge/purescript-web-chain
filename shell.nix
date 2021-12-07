c078b74f58566b3ec1901f4025f173e602c553e2
David M. Sledge <sledged@gmail.com>
2021-12-06 22:32:40 -0700

    Tweaking shell.nix

--- shell.nix
+++ shell.nix
@@ -1,27 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  easy-ps = import
-    (pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "5716cd791c999b3246b4fe173276b42c50afdd8d";
-      sha256 = "1r9lx4xhr42znmwb2x2pzah920klbjbjcivp2f0pnka7djvd2adq";
-    }) {
-    inherit pkgs;
-  };
-in
+
 pkgs.mkShell {
-  buildInputs = [
-    easy-ps.purs-0_14_4
-    easy-ps.psc-package
-    easy-ps.pulp
-    easy-ps.psc-package
-    pkgs.nodejs-14_x
-    pkgs.nodePackages.bower
-  ];
-  LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
-  # https://github.com/purescript/spago#install-autocompletions-for-bash
-  shellHook = ''
-    source <(spago --bash-completion-script `which spago`)
-  '';
+  buildInputs = (with pkgs; [
+    spago
+    pulp
+    nodejs-14_x
+    nodePackages.bower
+    purescript
+  ]);
 }
